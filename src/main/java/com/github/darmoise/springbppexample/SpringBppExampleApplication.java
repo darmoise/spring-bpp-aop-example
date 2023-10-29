0b861884422240f74616ced86c5b98f4180e0e3e
darmoise <darmoise@localhost>
2023-10-29 14:11:40 +0300

    init commit

---
+++ src/main/java/com/github/darmoise/springbppexample/SpringBppExampleApplication.java
@@ -0,0 +1,13 @@
+package com.github.darmoise.springbppexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBppExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBppExampleApplication.class, args);
+    }
+
+}
