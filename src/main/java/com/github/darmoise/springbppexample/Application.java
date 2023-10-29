fb4c1ac5b3978d2ea8fdc6e1f31faa87e80a89a6
darmoise <darmoise@localhost>
2023-10-29 15:28:31 +0300

    Переименовал SpringBppExampleApplication в Application

--- src/main/java/com/github/darmoise/springbppexample/Application.java
+++ src/main/java/com/github/darmoise/springbppexample/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBppExampleApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBppExampleApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
