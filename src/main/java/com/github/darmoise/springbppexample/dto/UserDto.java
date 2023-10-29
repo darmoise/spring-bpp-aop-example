70061cd2f0d25bf39081d36089fa2347ea2801b6
darmoise <darmoise@localhost>
2023-10-29 16:28:03 +0300

    Заменил id на хэш и преобразовал class в record

--- src/main/java/com/github/darmoise/springbppexample/dto/UserDto.java
+++ src/main/java/com/github/darmoise/springbppexample/dto/UserDto.java
@@ -1,14 +1,7 @@
 package com.github.darmoise.springbppexample.dto;
 
-import java.util.UUID;
-import lombok.Data;
-
-@Data
-public class UserDto {
-    private UUID id = UUID.randomUUID();
-    private String email;
-
-    public UserDto(final String email) {
-        this.email = email;
-    }
+public record UserDto(
+    String email,
+    Integer hash
+) {
 }
--- src/main/java/com/github/darmoise/springbppexample/service/UserService.java
+++ src/main/java/com/github/darmoise/springbppexample/service/UserService.java
@@ -1,18 +1,26 @@
 package com.github.darmoise.springbppexample.service;
 
 import com.github.darmoise.springbppexample.dto.UserDto;
-import com.github.darmoise.springbppexample.util.InjectRandomEmail;
+import com.github.darmoise.springbppexample.util.HashGenerator;
+import com.github.darmoise.springbppexample.util.annotation.InjectRandomEmail;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 @Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
 public class UserService {
+    private final HashGenerator<Integer> hashGenerator;
+
     @InjectRandomEmail(hosting = "mail.ru")
     private String email;
 
     public UserDto getUser() {
-        return new UserDto(email);
+        return new UserDto(
+            email,
+            hashGenerator.hash(email)
+        );
     }
 }
